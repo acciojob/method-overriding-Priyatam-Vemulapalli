4daeeb4d36aca428c8bb55ebcf961dd3bbbbff97
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-07-31 10:10:31 +0530

    final

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,9 +4,5 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    @Test
-    public void testB() {
-        com.driver.B b = new com.driver.B();
-        Assertions.assertEquals("Method is overridden in Extended class B", b.meth());
-    }
+    
 }
